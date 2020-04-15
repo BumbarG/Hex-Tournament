277ca0cbdc9804c5381f0c031398615547e7c23e
Gal <gal.bumbar@gmail.com>
2020-04-15 12:27:57 +0200

    fixed length in Dijkstra, doesnt work yet.

--- hex/libs/graph.py
+++ hex/libs/graph.py
@@ -10,14 +10,12 @@ from system.state import State
 def find_shortest_path_Dijkstra(player, game: State):
     '''
     returns the distance (int, min number of steps) between start (hex) and end (hex) in the game.
-    active_player ... not same as active player. 
+    player ... not same as active player. 
     game ... instance of State ... contains the tile graph and the player who is currently active.
-    start ... the starting cell.
-    end ... the target cell.
     if the function returns np.inf, then no path was found and a player should have already been declared the winnder.
     '''
     Q = heapdict.heapdict()
-    # prev = defaultdict(None) # we dont need the path
+    # prev = defaultdict(None)  # we dont need the path
     dist = defaultdict(lambda: np.inf)
     for i in range(game.size):
         for j in range(game.size):
@@ -35,27 +33,43 @@ def find_shortest_path_Dijkstra(player, game: State):
         if is_target(u[0], player, game.size):
             return u[1]
         for neighbour in get_neighbours(u[0], game.size):
-            if neighbour in Q:
-                alt = u[1] + length(u[0], neighbour, game.board)
-                if alt < dist[neighbour]:
-                    dist[neighbour] = alt
-                    Q[neighbour] = alt
-                    # prev[neighbour] = u[0] # we dont need the path
+            if game.board[neighbour[0], neighbour[1]] == player.value or game.board[neighbour[0], neighbour[1]] == Player.NONE.value:
+                if neighbour in Q:
+                    alt = u[1] + length(u[0], neighbour, game.board, player)
+                    if alt < dist[neighbour]:
+                        dist[neighbour] = alt
+                        Q[neighbour] = alt
+                        # prev[neighbour] = u[0]  # we dont need the path
 
     raise Exception('Function should terminate beforehand.')
 
 
-def length(a, b, board):
+def length(a, b, board, player):
     # have to handle imaginary fields.
     # None should not occur otherwise. It should only occur for the border fields.
-    if a[0] is None or a[1] is None:
+    if a[0] is -1:  # player we are calculating for is WHITE
+        if board[b[0], b[1]] == Player.WHITE.value:
+            return 0
+        if board[b[0], b[1]] == Player.BLACK.value:
+            return np.inf
+        else:
+            return 1
+
+    if a[1] is -1:  # player we are calculating for is BLACK
+        if board[b[0], b[1]] == Player.BLACK.value:
+            return 0
+        if board[b[0], b[1]] == Player.WHITE.value:
+            return np.inf
+        else:
+            return 1
+
+    if board[b[0], b[1]] == player.value:  # we are stepping onto our hex
         return 0
-    if board[a[0], a[1]] == board[b[0], b[1]]:
-        return 0
-    if (board[a[0], a[1]] == Player.NONE.value) or (board[b[0], b[1]] == Player.NONE.value):
+
+    if board[b[0], b[1]] == Player.NONE.value:  # we are stepping onto an empty hex
         return 1
-    else:
-        return np.inf
+
+    return np.inf
 
 
 def is_target(u, player, size):
